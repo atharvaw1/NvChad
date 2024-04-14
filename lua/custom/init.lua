2e3010f9f74f17b9c0e695efc2afdfbb0e7a5e32
atharvaw1 <ath.wand@gmail.com>
2024-04-14 16:06:57 -0400

    Highlight yank and codeium keybinds

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,12 @@
 vim.opt.relativenumber = true
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 vim.opt.scrolloff = 10
+-- highlight_yank
+vim.cmd [[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+augroup END
+]]
+-- Disable codeium tab auto
+vim.g.codeium_no_map_tab = true
